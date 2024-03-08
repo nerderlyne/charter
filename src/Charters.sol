2aafd4314ebea126c78a58ce6c69aa3f391b8339
shiv <shivanshityagi@icloud.com>
2024-03-08 21:20:04 +0530

    Update Charters.sol

--- src/Charters.sol
+++ src/Charters.sol
@@ -2,13 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract Charters {
-    mapping(address => bytes) private charters;
+    mapping(address => bytes) public charters;
 
     event Charter(address indexed owner, bytes data);
-
-    function charter(address _address) public view returns (bytes memory) {
-        return charters[_address];
-    }
 
     fallback() external payable {
         charters[msg.sender] = msg.data;
