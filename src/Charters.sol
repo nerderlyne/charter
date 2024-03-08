076ada21c1e6b94ffd3538c3b291e24baa8b0d5e
ross <92001561+z0r0z@users.noreply.github.com>
2024-03-08 15:53:28 +0000

    ~~

--- src/Charters.sol
+++ src/Charters.sol
@@ -1,13 +1,13 @@
-// SPDX-License-Identifier: AGPL 
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.24;
 
+/// @notice Onchain charter solutions.
 contract Charters {
-    mapping(address => bytes) public charters;
+    mapping(address owner => bytes data) public charters;
 
     event Charter(address indexed owner, bytes data);
 
     fallback() external payable {
-        charters[msg.sender] = msg.data;
-        emit Charter(msg.sender, msg.data);
+        emit Charter(msg.sender, charters[msg.sender] = msg.data);
     }
 }
